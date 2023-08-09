6a3ea79a6bac936ff180c8cf085d9728df98569a
Sakshi Palkar <Sakshipalkar1998@gmail.com>
2023-08-09 09:02:09 +0530

    ec2 delete

--- first_ec2_instance/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region     = "us-east-1"
-  
-}
-
- #resource "aws_iam_user" "my_user" {
-  #name = "loadbalancer"
- 
-#}
--- first_ec2_instance/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "instance_type" {
-
-    default = "t2.micro"# variable default value
-    #from terminal command terraform plan -var="instance_type=t2.small"
-    # if {}is blank then we can give value from ternminal as well like it shows option 
-    #Enter the value
-    
-  
-}
